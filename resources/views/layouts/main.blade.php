93cc10b2341ba964b3ddbd1d1794ab103ec61eef
jackmcdowell <jackmcdowell@gmail.com>
2015-10-22 12:57:43 -0700

    setting up resources

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Foundation | Welcome</title>
         <link rel="stylesheet" href="{{ asset('css/foundation.css') }}" />
-        <script src="js/vendor/modernizr.js"></script>
+        <script src="{{ asset('js/vendor/modernizr.js') }}"></script>
     </head>
 
 <body>
@@ -41,9 +41,8 @@
             </div>
         </div>
     </footer>
-
-    <script src="js/vendor/jquery.js"></script>
-    <script src="js/foundation.min.js"></script>
+    <script src="{{ asset('js/vendor/jquery.js') }}"></script>
+    <script src="{{ asset('js/foundation.min.js') }}"></script>
     <script>
       $(document).foundation();
     </script>
